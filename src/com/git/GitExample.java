17a6cf983e0972feaac4bbdcd9cca2f1b1d9f910
Anil-139 <60874786+Anil-139@users.noreply.github.com>
2020-06-25 23:01:20 +0530

    This is my first commit

---
+++ src/com/git/GitExample.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("print-1");
+		
+		System.out.println("print-2");
+		
+		System.out.println("print-3");
+		
+		
+		
+	}
+
+}
